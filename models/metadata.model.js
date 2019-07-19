79e847015f6a35a64d2eb7d5083f7a212da7f12b
NAPR\mjaparidze <mjaparidze@napr.local>
2019-07-19 19:08:07 +0400

    metadata model launched

--- models/metadata.model.js
+++ models/metadata.model.js
@@ -5,10 +5,40 @@ let validator = require('validator');
 
 
 const Schema = mongoose.Schema;
+
+const commentsSchema = new Schema({
+    comment:{
+        type:String,
+        required:false
+    },
+    commentDate:{
+        type: { type: Date, default: Date.now },
+    }
+})
+
+
+const resource_name = new Schema({
+    comments:[commentsSchema],
+    resourceName:{
+        type:String,
+        required: true
+    }
+},{})
+
+
 const metadataSchema = new Schema({
+    userId: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3
+    },
+    resource_name: resource_name,
 
 
 
-
+    },{
+        timestamps: true
     }
 );
